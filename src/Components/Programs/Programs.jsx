f60853b85d7f69b4c2c3c7422af1dda732ff53e9
Prince0206 <124934940+Prince0206@users.noreply.github.com>
2025-01-29 00:36:14 +0000

    Work update

--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Programs.css'
-import program1 from '../../assets/program1.jpeg'
-import program2 from '../../assets/program2.jpeg'
-import program3 from '../../assets/program3.jpeg'
+import Program1 from '../../assets/Program1.jpeg'
+import Program2 from '../../assets/Program2.jpeg'
+import Program3 from '../../assets/Program3.jpeg'
 import program_icon from '../../assets/program_icon.webp'
 import program_icon2 from '../../assets/program_icon2.webp'
 import program_icon3 from '../../assets/program_icon3.jpeg'
@@ -14,7 +14,7 @@ const Programs = () => {
     <div className='programs' id='program'>
     
         <div className='program'>
-            <img src={program1} alt='' />
+            <img src={Program1} alt='' />
             <div className='caption'>
                 <img src={program_icon} alt='' />
                 <p>Our primary school program is designed to help your child develop the necessary skills to excel in their academics and personal development</p>
@@ -22,7 +22,7 @@ const Programs = () => {
         </div>
         <div className='program'>
             
-            <img src={program2} alt='' />
+            <img src={Program2} alt='' />
             <div className='caption'>
                 <img src={program_icon2} alt='' />
                 <p>Our secondary school program is designed to help your child develop the necessary skills to excel in their academics and personal development</p>
@@ -30,7 +30,7 @@ const Programs = () => {
         </div>
         <div className='program'>
            
-            <img src={program3} alt='' />
+            <img src={Program3} alt='' />
             <div className='caption'>
                 <img src={program_icon3} alt='' />
                 <p>Our high school program is designed to help your child develop the necessary skills to excel in their academics and personal development</p>
